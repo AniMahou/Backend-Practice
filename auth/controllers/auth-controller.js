b69f01b134e3278b9157f6afaab64a0a60cda99c
ZoldyckX <tabibhassan.1604@gmail.com>
2026-01-29 20:31:37 +0600

    auth-controller finish

--- auth/controllers/auth-controller.js
+++ auth/controllers/auth-controller.js
@@ -1,6 +1,41 @@
+import User from "../models/User"
+const bcrypt = require('bcryptjs')
 const registerUser = async(req,res)=>{
     try{
+        const {username, email, password,role} =req.body
+        const checkExistingUser = await User.findOne({$or : [{username}, {email}]})
 
+        if(checkExistingUser){
+            res.status(400).json({
+                success: false,
+                message: 'User exists with same username/mail'
+            })
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(password, salt)
+
+        const newUser = new User ({
+            username,
+            email,
+            password: hashedPass,
+            role: role || 'user'
+
+        })
+        await newUser.save()
+
+        if(newUser){
+            res.status(201).json({
+                success:true,
+                message: 'User created'
+            })
+        }
+        else{
+            res.status(400).json({
+                success: false,
+                message: 'User created'
+            })
+        }
     }catch(e){
         res.status(500).json({
             success: false,
@@ -8,6 +43,7 @@ const registerUser = async(req,res)=>{
         })
     }
 }
+
 
 const loginUser = async() => {
     try{
